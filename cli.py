655541534d0c39631dbf5ac5f4aca6f8fe918f35
Teppei-Kanayama <kanayama.teppei@gmail.com>
2023-03-18 17:54:50 +0900

    リファクタ

--- cli.py
+++ cli.py
@@ -3,30 +3,30 @@ import os
 import json
 import sys
 
-if __name__ == '__main__':
-    prompt = sys.stdin.read()
-
-    url = "https://api.openai.com/v1/chat/completions"
-    headers = {
+URL = "https://api.openai.com/v1/chat/completions"
+HEADERS = {
         "Content-Type": "application/json",
         "Authorization": "Bearer " + os.environ.get('OPENAI_API_KEY')
     }
+
+def format_message(message):
+    if not 'GPT >' in message:
+        return '\nGPT > ' + message + '\n\n' + '私 > '
+    return '\n' + message + '\n\n' + '私 > '
+
+if __name__ == '__main__':
+    prompt = sys.stdin.read()
     data = {
         "model": "gpt-3.5-turbo",
         "messages": [{"role": "user", "content": prompt}],
         "max_tokens": 1024,
     }
-
-    response = requests.post(url, headers=headers, data=json.dumps(data))
+    response = requests.post(URL, headers=HEADERS, data=json.dumps(data))
 
     if response.ok:
         result = json.loads(response.content)
         message = result['choices'][0]['message']['content']
-
-        if not 'GPT >' in message:
-            message = 'GPT > ' + message
-
         with open('input.md', 'a') as f:
-            f.write('\n' + message + '\n\n' + '私 > ')
+            f.write(format_message(message))
     else:
         print(response.content)
